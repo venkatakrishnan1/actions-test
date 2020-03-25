57cef907bce7a1091d4f61a53d20bb822e9d1ece
Venkatakrishnan Jayakumar <jvenkatakrishnan@pkrmsezdc.com>
2020-03-25 15:48:50 +0530

    second commit

---
+++ src/main/java/com/example/githubaction/GithubactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionApplication.class, args);
+	}
+
+}
